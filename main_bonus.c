c0bc95dd0e0accaa2dbda8b0f550ff379e525fa4
benjaminmerchin <benjaminmerchin@gmail.com>
2020-11-04 03:16:06 +0100

    yup

--- main_bonus.c
+++ main_bonus.c
@@ -23,11 +23,37 @@ int main(int ac, char **av)
 	int retour;
 	char *line;
 
+	if (ac != 3)
+	{
+		ft_putstr(" Merci de mettre 2 fichiers en entree");
+		return (0);
+	}
 	i = 0;
 	line = NULL;
-	if (ac != 2)
-		return (0);
 	if ((fd = open(av[1], O_RDONLY)) < 0)
+	{
+		write(2, "OpenError\n", 10);
+		return (0);
+	}
+	while ((retour = get_next_line(fd, &line)) == 1)
+	{
+		ft_putstr(line);
+		free(line);
+		i++;
+		/*if (i == 10)
+			break ;*/
+//		write(1, "1", 1);
+	}
+	if (line[0] != 0)
+	{
+		ft_putstr(line);
+		free(line);
+	}
+
+
+	i = 0;
+	line = NULL;
+	if ((fd = open(av[2], O_RDONLY)) < 0)
 	{
 		write(2, "OpenError\n", 10);
 		return (0);
